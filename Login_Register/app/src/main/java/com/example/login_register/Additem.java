d35ee31a6b539001ae74ed30417a4b3c99c1a4b3
Suraj <surajsubrahmanyn@gmail.com>
2023-09-04 00:02:31 +0530

    commit

--- Login_Register/app/src/main/java/com/example/login_register/Additem.java
+++ Login_Register/app/src/main/java/com/example/login_register/Additem.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.List;
 
 public class Additem extends AppCompatActivity {
-    EditText e1,e2;
+    TextView t6,t7,t8;
     CDB db;
 
     @Override
@@ -19,10 +20,40 @@ public class Additem extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.buger);
         db=new CDB(this);
-        e1=(EditText)findViewById(R.id.e1);
-        e2=(EditText)findViewById(R.id.e2);
-    }
-    public void Additem(View v){
+        t6=(TextView)findViewById(R.id.t6);
+        t7=(TextView)findViewById(R.id.t7);
+        t8=(TextView)findViewById(R.id.t8);
 
+    }
+    public void AddCart(View v){
+
+    }
+
+    public void Add(View v){
+        String s3= t7.getText().toString();
+        int i=Integer.parseInt(s3);
+        i++;
+        String str=String.valueOf(i);
+        t7.setText(str);
+        String s2= t6.getText().toString();
+        int n2=Integer.parseInt(s2);
+        int n=i*n2;
+        String tp=String.valueOf(n);
+        t8.setText(tp);
+    }
+
+    public void Minus(View v){
+        String s4= t7.getText().toString();
+        int i=Integer.parseInt(s4);
+        if(i!=0) {
+            i--;
+            String str = String.valueOf(i);
+            t7.setText(str);
+            String s2= t6.getText().toString();
+            int n2=Integer.parseInt(s2);
+            int n=i*n2;
+            String tp=String.valueOf(n);
+            t8.setText(tp);
+        }
     }
 }
---
+++ Login_Register/app/src/main/java/com/example/login_register/CDBcart.java
@@ -0,0 +1,110 @@
+package com.example.login_register;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CDBcart extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION=1;
+    private static final String DATABASE_NAME="DMS";
+    CDBcart(Context context){
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table cart(dno integer primary key autoincrement,foodname text,dloc text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("drop table if exists login");
+        onCreate(db);
+    }
+    public void Insert(CLogin cl){
+        try{
+            SQLiteDatabase db=this.getWritableDatabase();
+            ContentValues cv=new ContentValues();
+            cv.put("name",cl.name);
+            cv.put("username",cl.username);
+            cv.put("password",cl.password);
+            db.insert("login",null,cv);
+            db.close();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+//    public List<CLogin> getAllvalues(String un,String pw){
+//        List<CLogin> recList = new ArrayList<CLogin>();
+//        String selectQuery="select * from login where username='"+un+"' and password='"+pw+"'";
+//        SQLiteDatabase db=this.getReadableDatabase();
+//        Cursor cursor=db.rawQuery(selectQuery,null);
+//        if(cursor.moveToFirst()){
+//            do{
+//                CLogin rec=new CLogin();
+//                rec.name=cursor.getString(0);
+//                rec.username=cursor.getString(1);
+//                rec.password=cursor.getString(2);
+//                recList.add(rec);
+//
+//            }while(cursor.moveToNext());
+//        }
+//        return recList;
+//    }
+
+//    public List<CLogin> getvalues(String un){
+//        List<CLogin> recList = new ArrayList<CLogin>();
+//        String selectQuery="select * from login where username='"+un+"'";
+//        SQLiteDatabase db=this.getReadableDatabase();
+//        Cursor cursor=db.rawQuery(selectQuery,null);
+//        if(cursor.moveToFirst()){
+//            do{
+//                CLogin rec=new CLogin();
+//                rec.name=cursor.getString(0);
+//                rec.username=cursor.getString(1);
+//                rec.password=cursor.getString(2);
+//                recList.add(rec);
+//
+//            }while(cursor.moveToNext());
+//        }
+//        return recList;
+//    }
+//
+//
+//    public List<CLogin> Search(){
+//        List<CLogin> recList = new ArrayList<CLogin>();
+//        String selectQuery="select * from login";
+//        SQLiteDatabase db=this.getReadableDatabase();
+//        Cursor cursor=db.rawQuery(selectQuery,null);
+//        if(cursor.moveToFirst()){
+//            do{
+//                CLogin rec=new CLogin();
+//
+//                rec.username=cursor.getString(1);
+//
+//                recList.add(rec);
+//
+//            }while(cursor.moveToNext());
+//        }
+//        return recList;
+//    }
+//
+//    public int deleteuser(String username)
+//    {
+//        SQLiteDatabase db=this.getWritableDatabase();
+//        return db.delete("login","username=?",new String[] {String.valueOf(username)});
+//    }
+//
+//    public void update(String np,String un){
+//        SQLiteDatabase db=this.getWritableDatabase();
+//        ContentValues values =  new ContentValues();
+//        values.put("password",np);
+//        db.update("login",values,"username=?",new String[]{String.valueOf(un)});
+//        db.close();
+//    }
+}
