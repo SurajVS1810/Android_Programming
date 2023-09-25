86a296d98c467bdbadbf6e109a06fc9a419e384b
Suraj V S <111676009+SurajUnny@users.noreply.github.com>
2023-09-25 14:27:19 +0530

    commit

---
+++ FileApp/app/src/main/java/com/example/fileapp/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.fileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onSave(View v){
+        try{
+            EditText e1=(EditText)findViewById(R.id.ed1);
+            EditText e2=(EditText)findViewById(R.id.ed2);
+            FileOutputStream fout;
+            fout=openFileOutput(e1.getText().toString(),0);
+            OutputStreamWriter osw=new OutputStreamWriter(fout);
+            osw.write(e2.getText().toString());
+            osw.flush();
+            osw.close();
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void onOpen(View v){
+        try{
+            EditText e1=(EditText)findViewById(R.id.ed1);
+            EditText e2=(EditText)findViewById(R.id.ed2);
+            FileInputStream fin;
+            fin=openFileInput(e1.getText().toString());
+            InputStreamReader isw=new InputStreamReader(fin);
+            char[] b=new char[10];
+            int n=isw.read(b,0,10);
+            String str=new String(b,0,n);
+            e2.setText(str);
+            isw.close();
+
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
